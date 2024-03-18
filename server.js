a9748c5bdfe8489b8eee93522fc06a1f0236ac75
Tsgcode1122 <Tsgcode1122@users.noreply.github.com>
2024-03-18 01:09:17 +0100

    hy

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cors());
 app.use(
   cors({
-    origin: "https://tossolhotel.onrender.com",
+    origin: "https://tossolhotel.onrender.com/",
   }),
 );
 // Routes
